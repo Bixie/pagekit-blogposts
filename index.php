8262f4a129d5619f3e88d6bdde237aec97e5b46a
malles <matthijs@bixie.nl>
2016-03-21 13:44:44 +0100

    truncate to factory

--- index.php
+++ index.php
@@ -6,11 +6,25 @@ return [
 
 	'type' => 'extension',
 
-	'autoload' => [
-
-		'Bixie\\Blogposts\\' => 'src'
-
-	],
+	'main' => function ($app) {
+		$app->factory('string.truncate', function ($app) {
+			return function ($text, $length = 200) {
+				$text = strip_tags($text);
+				if ($length) {
+					if (function_exists('mb_strpos')) {
+						if (($pos = @mb_strpos($text, ' ', $length)) > 0) {
+							$text = mb_substr($text, 0, $pos) . '...';
+						}
+					} else {
+						if (($pos = @strpos($text, ' ', $length)) > 0) {
+							$text = substr($text, 0, $pos) . '...';
+						}
+					}
+				}
+				return $text;
+			};
+		});
+	},
 
 	'widgets' => [
 
